71cbbe63f608412d7ae4fab2c9b27b2d4ef5b394
Yiger Lopez <58009086+YigerLP@users.noreply.github.com>
2022-10-03 13:41:24 -0600

    Tarea #1 de Diseño Web y Patrones

---
+++ src/main/java/com/informacion/InformacionApplication.java
@@ -0,0 +1,13 @@
+package com.informacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InformacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InformacionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/informacion/InformacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.informacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InformacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
